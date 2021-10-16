25c4b6910a8000f062a0c34c7f42fdcae3092c21
ayoub <ayoub.belghar@gmail.com>
2021-10-16 14:58:26 +0200

    add prod pipeline deployment for parent micro app

---
+++ parent/config/webpack.prod.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const jsonPackage = require('../package.json');
+
+const configProd = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/parent/latest/'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'parent',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './ParentManager': './src/bootstrap'
+            },
+            shared: jsonPackage.dependencies
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, configProd);
